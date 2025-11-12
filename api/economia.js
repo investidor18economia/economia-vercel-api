e3e891ebebfd18cb0d228c9d296c0948ffbbb168
investidor18economia <val18jo@gmail.com>
2025-11-11 23:07:09 -0300

    Update economia.js

--- api/economia.js
+++ api/economia.js
@@ -1,4 +1,11 @@
-import fetch from 'node-fetch';
-export default async function handler(req, res) {
-  ...
+export default function handler(req, res) {
+  res.status(200).json({
+    status: "ok",
+    message: "Rota de economia funcionando!",
+    data: {
+      PIB: "3.2%",
+      inflação: "4.5%",
+      desemprego: "7.8%",
+    },
+  });
 }
