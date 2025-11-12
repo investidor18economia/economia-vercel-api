78394ff14dd7a559a8e178f65c06ee8a6251d59a
investidor18economia <val18jo@gmail.com>
2025-11-11 22:40:17 -0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "Arial", padding: "40px" }}>
-      <h1>🚀 API de Economia — online!</h1>
-      <p>Esta é a página inicial do projeto hospedado no Vercel.</p>
-    </div>
-  );
-}
