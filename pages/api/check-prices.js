f3b56d97e7fcaacead4d0b4662612205709af840
investidor18economia <val18jo@gmail.com>
2026-04-23 19:50:39 -0300

    Update check-prices.js

--- pages/api/check-prices.js
+++ pages/api/check-prices.js
@@ -37,7 +37,7 @@ async function fetchFromSerpApi(query) {
 export default async function handler(req, res) {
   return res.status(200).json({
     success: true,
-    message: "API funcionando sem fetch"
+    message: "API funcionando sem fetch";
   });
 }
 
